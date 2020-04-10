22db64b8af7bbcb07e75635ffb41a9af965843ce
masterful-array-3850 <crgammage@gmail.com>
2020-04-10 14:31:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {"Alison": "Slowdive", "Sometimes": "My Bloody Valentine"}
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+  playlist["Phil Ochs"] = "Here's to the State of Mississippi"
   return playlist
 }
