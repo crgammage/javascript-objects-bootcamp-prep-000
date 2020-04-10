27040096e8cdb7af6a3ee5938c75e2b00e8b1d49
masterful-array-3850 <crgammage@gmail.com>
2020-04-10 14:18:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {[]"Alison"]: "Alison", ["Sometimes"]: "My Bloody Valentine"}
-function destructivelyUpdatePlaylist(playlist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {["Kygo"]: "Higher Love"});
 }
